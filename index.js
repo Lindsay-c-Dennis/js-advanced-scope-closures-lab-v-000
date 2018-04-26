39d1dea2b39f35157ccfde55989e195c56e0f05b
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-26 17:50:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ function produceDrivingRange(blockRange) {
   return function(startBlock, endBlock) {
     let start = parseInt(startBlock);
     let end = parseInt(endBlock);
-    let distance= Math.abs()
-    Math.abs(parseint(endBlock) - parseInt(startBlock)) <= blockRange;
+    let distance = Math.abs(end - start);
+    
+    
+    
   };
 }
