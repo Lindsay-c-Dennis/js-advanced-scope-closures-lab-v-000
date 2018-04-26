250f88b6a6a919e01718c86b41c92bf641ad3335
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-26 17:51:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(blockRange) {
   return function(startBlock, endBlock) {
     let start = parseInt(startBlock);
     let end = parseInt(endBlock);
-    let distance = Math.abs(end - start);
+    let travelDistance = Math.abs(end - start);
     
     
     
