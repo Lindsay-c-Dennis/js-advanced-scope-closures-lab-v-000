ad970dc83a4cc4acdc9478eab33797baa1d927aa
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-26 17:45:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function produceDrivingRange(blockRange) {
   return function withinRange(distance) {
-    if (distance <= blockRange);
+    if (distance <= blockRange) {
       buffer = (blockRange - distance);
       return `within range by ${buffer} blocks.`;
     else
       diff = (distance - blockRange);
       return `${diff} block out of range`;
-  };
+      }
+  }
 }
